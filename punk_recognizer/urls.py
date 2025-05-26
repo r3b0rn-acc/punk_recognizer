d51f2cb713eedb76a0cef0a3a6c1276e7039d161
r3b0rn <yakovinart123@gmail.com>
2025-05-26 06:48:58 +0300

    🚨 chore: removed unused 'index' view and its import in __init__.py, updated urls with TemplateView

--- punk_recognizer/urls.py
+++ punk_recognizer/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-
-import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
