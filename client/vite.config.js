68b972b1bcf36e8b6419ccb131f57a4a591c5926
r3b0rn <yakovinart123@gmail.com>
2025-05-27 23:43:34 +0300

    ✨ feat: added Tailwind CSS support in project

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite';
 
 
 export default defineConfig({
   plugins: [
     vue(),
+    tailwindcss(),
   ],
   server: {
     origin: 'http://localhost:5173'
