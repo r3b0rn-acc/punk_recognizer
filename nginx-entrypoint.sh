0433f8791e33bb2a769b62ccdf51f6d01ba9136b
r3b0rn <yakovinart123@gmail.com>
2025-05-27 04:57:29 +0300

    🐛 fix: added SSL certificate variables passed to the envsubst environment

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -8,7 +8,7 @@ else
   TEMPLATE=/etc/nginx/templates/nginx.dev.template
 fi
 
-envsubst '$SERVER_NAME' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
+envsubst '$SERVER_NAME $SSL_CERTIFICATE $SSL_CERTIFICATE_KEY' < "$TEMPLATE" > /etc/nginx/conf.d/default.conf
 
 echo "Launching nginx"
 exec nginx -g 'daemon off;'
